5a90b62ca795d3c33d4ce36c0461c634db33e0e1
sandhyakamath <sandhya_kammath@yahoo.co.in>
2025-05-31 19:33:27 +0100

    activity completed

---
+++ activity3.py
@@ -0,0 +1,9 @@
+def numberofBits(n):
+    count = 0
+    while (n):
+        count +=1
+        n >>= 1
+    return count
+
+n = int(input("Enter a number: "))
+print("number of bits: ", numberofBits(n))
