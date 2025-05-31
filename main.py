250dad4e0655b2d7ff188c3d07d603b24798ba9a
sandhyakamath <sandhya_kammath@yahoo.co.in>
2025-05-31 19:25:27 +0100

    activity completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ num1 = 10
 num2 = 4 
 print("num1 & num2 are:", num1, num2)
 print("num1 | num2 are:", num1 | num2)
-print("num1  num2 are:", num1  num2)
+print("num1 ^ num2 are:", num1 ^ num2)
 print("num1 << num2 are:", num1 << num2)
 print("num1 >> num2 are:", num1 >> num2)
 print("~num1 are:", ~num1)
